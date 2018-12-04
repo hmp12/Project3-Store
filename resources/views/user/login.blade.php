8f7f0c4d2a4f02bac8dc092309f5636d946034a8
hmp12 <phongpt97@gmail.com>
2018-12-04 19:27:41 +0700

    Reduce css code user

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -7,26 +7,20 @@
 	</head>	
 
 	<body>
-		<div class="login">
-			<div class="top_box">
-				<img src="{{ url('/') }}/img/gaugau.png" height="100%">
-			</div>
-			<form action="{{ action('UserController@login') }}" method="post" class="login_box">
-				@csrf
-				<span class="error">{{ (isset($error)) ? $error : ''}}</span>
-				<h4>User name: </h4>
-				<input type="text" placeholder="Enter user name" value="{{ (isset($username)) ? $username : ''}}" name="username" id="usr">
-				<h4>Password: </h4>
-				<input type="password" placeholder="Enter password" value="{{ (isset($pass)) ? $pass : ''}}" name="pass" id="pass">
-				<button type="submit" id="login_bt">Login</button>
-				<button type="button" onclick="location.href='{{ url('/') }}/user/signup'" id="signup_bt">Signup</button>
-			</form>	
-			<div class="bottom_box">
-				<p class="inline">Sign in with: </p>
-				<img src="../img/google.svg" height="60%" class="logo">
-				<img src="../img/facebook.svg" height="60%" class="logo">
-				<img src="../img/twitter.svg" height="60%" class="logo">
-			</div>		
+		<div class="container">
+			<div class="row">
+				<form action="{{ action('UserController@login') }}" method="post" class="login-box col-5 offset-3">
+					@csrf
+					<h3 class="text-center">Login</h3>
+					<span class="error">{{ (isset($error)) ? $error : ''}}</span>
+					<p>User name: </p>
+					<input type="text" class="form-control" placeholder="Enter user name" value="{{ (isset($username)) ? $username : ''}}" name="username" id="usr">
+					<p>Password: </p>
+					<input type="password" class="form-control" placeholder="Enter password" value="{{ (isset($pass)) ? $pass : ''}}" name="pass" id="pass">
+					<button type="submit" class="btn btn-success float-right" id="login_bt">Login</button>
+					<button type="button" class="btn btn-primary float-right" onclick="location.href='{{ url('/') }}/user/signup'" id="signup_bt">Signup</button>
+				</form>
+			</div>			
 		</div>
 	</body>
 </html>
--- resources/views/user/signup.blade.php
+++ resources/views/user/signup.blade.php
@@ -6,25 +6,26 @@
 
 	<body>
 		<div class="signup">
-			<form action="{{ action('UserController@signUp') }}" method="post" class="signup_box">
-				@csrf
-				<span class="error">* is required</span>
-				<p>Full name: <span class="error">* {{ (isset($fullnameError)) ? $fullnameError : ''}}</span></p>
-				<input type="text" placeholder="Enter your name" value="{{ (isset($fullname)) ? $fullname : ''}}" name="fullname" id="name">
-				<p>User name: <span class="error">* {{ (isset($usernameError)) ? $usernameError : ''}}</span></p>
-				<input type="text" placeholder="Enter user name" value="{{ (isset($username)) ? $username : ''}}" name="username" id="usr">
-				<p>Email: <span class="error">* {{ (isset($emailError)) ? $emailError : ''}}</p>
-				<input type="email" placeholder="Enter user name" value="{{ (isset($email)) ? $email : ''}}" name="email" id="email">
-				<p>Password: <span class="error">* {{ (isset($passError)) ? $passError : ''}}</p>
-				<input type="password" placeholder="Enter password" name="pass" id="pass">
-				<p>Confirm Password: <span class="error">* {{ (isset($rePassError)) ? $rePassError : ''}}</p>
-				<input type="password" placeholder="ReEnter password" name="rePass" id="re_pass">
-				<p>Gender</p>
-				<input type="radio" name="gender" {{ (isset($gender) && $gender=="male") ? 'checked' : ''}} value="male">Male
-				<input type="radio" name="gender" {{ (isset($gender) && $gender=="female") ? 'checked' : ''}} value="female">Female
-				<button type="submit" id="signup_bt">Signup</button>
-			</form>		
-		</div>	
-	
+			<div class="row">
+				<form action="{{ action('UserController@signUp') }}" method="post" class="signup-box col-4 offset-4">
+					@csrf
+					<span class="error">* is required</span>
+					<p>Full name: <span class="error">* {{ (isset($fullnameError)) ? $fullnameError : ''}}</span></p>
+					<input type="text" class="form-control" placeholder="Enter your name" value="{{ (isset($fullname)) ? $fullname : ''}}" name="fullname" id="name">
+					<p>User name: <span class="error">* {{ (isset($usernameError)) ? $usernameError : ''}}</span></p>
+					<input type="text" class="form-control" placeholder="Enter user name" value="{{ (isset($username)) ? $username : ''}}" name="username" id="usr">
+					<p>Email: <span class="error">* {{ (isset($emailError)) ? $emailError : ''}}</p>
+					<input type="email" class="form-control" placeholder="Enter user name" value="{{ (isset($email)) ? $email : ''}}" name="email" id="email">
+					<p>Password: <span class="error">* {{ (isset($passError)) ? $passError : ''}}</p>
+					<input type="password" class="form-control" placeholder="Enter password" name="pass" id="pass">
+					<p>Confirm Password: <span class="error">* {{ (isset($rePassError)) ? $rePassError : ''}}</p>
+					<input type="password" class="form-control" placeholder="ReEnter password" name="rePass" id="re_pass">
+					<p>Gender</p>
+					<input type="radio" name="gender" {{ (isset($gender) && $gender=="male") ? 'checked' : ''}} value="male">Male
+					<input type="radio" name="gender" {{ (isset($gender) && $gender=="female") ? 'checked' : ''}} value="female">Female
+					<button type="submit" class="btn btn-primary float-right" id="signup_bt">Signup</button>
+				</form>
+			</div>	
+		</div>		
 	</body>
 </html>
